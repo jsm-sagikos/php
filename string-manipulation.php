20c13560395ee3de99cdedc9454d1e82bf50f3a6
jsm-sagikos <93094671+jsm-sagikos@users.noreply.github.com>
2022-02-17 12:11:15 +0100

    yup

--- string-manipulation.php
+++ string-manipulation.php
@@ -1,3 +1,28 @@
 <?php
 
 /* Useful functions and tips around string manipulation in PHP. */
+
+
+/**
+ * chr()
+ * 
+ * Returns a string for an ascii number, e.g. 49 = "1".
+ */
+    echo chr(49);       
+
+    /**
+     * Build an UTF-8 string from individual bytes by concatenating chrs:
+     */
+    $str = chr(240) . chr(159) . chr(144) . chr(152);
+    echo $str;
+
+
+/**
+ * ord()
+ * 
+ * Returns the ascii number for a string, e.g. "1" = 49.
+ */
+    echo ord("1");      
+
+
+
