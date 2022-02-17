ee86a8d7d575527ef26dabca1437fb077010177f
Joakim Saettem-Mucsi <joakim.saettem-mucsi@sagikos.com>
2022-02-17 12:16:07 +0100

    displaying examples

--- string-manipulation.php
+++ string-manipulation.php
@@ -8,13 +8,13 @@
  * 
  * Returns a string for an ascii number, e.g. 49 = "1".
  */
-    echo chr(49);       
+    echo "chr(49)=" . chr(49) . "\n";
 
     /**
      * Build an UTF-8 string from individual bytes by concatenating chrs:
      */
     $str = chr(240) . chr(159) . chr(144) . chr(152);
-    echo $str;
+    echo "chr(240).chr(159).chr(144).chr(152)=" . $str . "\n";
 
 
 /**
@@ -22,7 +22,7 @@
  * 
  * Returns the ascii number for a string, e.g. "1" = 49.
  */
-    echo ord("1");      
+    echo "ord(\"1\")=" . ord("1") . "\n";
 
 
 
